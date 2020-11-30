15dddaba5d3fd2224409a05e3848a7c9b4f8b5f2
Chris Givens <givenscj@Hotmail.com>
2020-11-30 12:16:59 -0800

    Update post-install-script01.ps1

--- artifacts/environment-setup/automation/spektra/post-install-script01.ps1
+++ artifacts/environment-setup/automation/spektra/post-install-script01.ps1
@@ -175,17 +175,23 @@ switch ($BuildEdition) {
 
 function InstallNotepadPP()
 {
-	#check for executables...
+	write-host "Installing Notepad++";
+    
+    #check for executables...
 	$item = get-item "C:\Program Files (x86)\Notepad++\notepad++.exe" -ea silentlycontinue;
 	
 	if (!$item)
 	{
-		$downloadNotePad = "https://notepad-plus-plus.org/repository/7.x/7.5.4/npp.7.5.4.Installer.exe";
+        $downloadNotePad = "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.9.1/npp.7.9.1.Installer.exe";
+        
+        #https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.9.1/npp.7.9.1.Installer.exe
 
-        mkdir c:\temp
+        mkdir c:\temp -ea silentlycontinue   
 		
 		#download it...		
-		Start-BitsTransfer -Source $DownloadNotePad -DisplayName Notepad -Destination "c:\temp\npp.exe"
+        #Start-BitsTransfer -Source $DownloadNotePad -DisplayName Notepad -Destination "c:\temp\npp.exe"
+        
+        Invoke-WebRequest $downloadNotePad -OutFile "c:\temp\npp.exe"
 		
 		#install it...
 		$productPath = "c:\temp";				
@@ -193,6 +199,180 @@ function InstallNotepadPP()
 		$argList = "/S"
 		start-process "$productPath\$productExec" -ArgumentList $argList -wait
 	}
+}
+
+function InstallUbuntu()
+{
+    write-host "Installing Ubuntu";
+
+    winrm quickconfig -force
+
+    $Path = "c:/temp";
+    Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1604 -OutFile "$path/Ubuntu1604.appx" -UseBasicParsing
+
+    powershell.exe -c "`$user='$localusername'; `$pass='$password'; try { Invoke-Command -ScriptBlock { Add-AppxPackage `"$path\Ubuntu1604.appx`" } -ComputerName localhost -Credential (New-Object System.Management.Automation.PSCredential `$user,(ConvertTo-SecureString `$pass -AsPlainText -Force)) } catch { echo `$_.Exception.Message }" 
+
+    Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1804 -OutFile "$path/Ubuntu1804.appx" -UseBasicParsing
+
+    powershell.exe -c "`$user='$localusername'; `$pass='$password'; try { Invoke-Command -ScriptBlock { Add-AppxPackage `"$path\Ubuntu1804.appx`" } -ComputerName localhost -Credential (New-Object System.Management.Automation.PSCredential `$user,(ConvertTo-SecureString `$pass -AsPlainText -Force)) } catch { echo `$_.Exception.Message }" 
+
+    Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-2004 -OutFile "$path/Ubuntu2004.appx" -UseBasicParsing
+
+    powershell.exe -c "`$user='$localusername'; `$pass='$password'; try { Invoke-Command -ScriptBlock { Add-AppxPackage `"$path\Ubuntu2004.appx`" } -ComputerName localhost -Credential (New-Object System.Management.Automation.PSCredential `$user,(ConvertTo-SecureString `$pass -AsPlainText -Force)) } catch { echo `$_.Exception.Message }" 
+
+    Add-AppxProvisionedPackage -Online -PackagePath C:\temp\Ubuntu1604.appx -skiplicense
+
+    cd 'C:\Program Files\WindowsApps\'
+    $installCommand = (Get-ChildItem -Path ".\" -Recurse ubuntu1604.exe)[0].Directory.FullName
+    $installCommand += "\Ubuntu1604.exe"
+    & $installCommand;
+
+    Add-AppxProvisionedPackage -Online -PackagePath C:\temp\Ubuntu1804.appx -skiplicense
+
+    $installCommand = (Get-ChildItem -Path ".\" -Recurse ubuntu1804.exe)[0].Directory.FullName + "\Ubuntu1804.exe"
+    & $installCommand;
+
+    Add-AppxProvisionedPackage -Online -PackagePath C:\temp\Ubuntu2004.appx -skiplicense
+
+    $installCommand = (Get-ChildItem -Path ".\" -Recurse ubuntu2004.exe)[0].Directory.FullName + "\Ubuntu2004.exe"
+    & $installCommand;
+}
+
+function InstallChrome()
+{
+    write-host "Installing Chrome";
+
+    $Path = "c:\temp"; 
+    $Installer = "chrome_installer.exe"; 
+    Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $Path\$Installer; 
+    Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait; 
+    Remove-Item $Path\$Installer
+}
+
+function InstallDockerDesktop()
+{
+    write-host "Installing Docker Desktop";
+
+    <#
+    mkdir c:\temp -ea silentlycontinue
+    #Docker%20Desktop%20Installer.exe install --quiet
+
+    $downloadNotePad = "https://desktop.docker.com/win/stable/Docker%20Desktop%20Installer.exe";
+
+    #download it...		
+    Start-BitsTransfer -Source $DownloadNotePad -DisplayName Notepad -Destination "c:\temp\dockerdesktop.exe"
+    
+    #install it...
+    $productPath = "c:\temp";				
+    $productExec = "dockerdesktop.exe"	
+    $argList = "install --quiet"
+
+    $credentials = New-Object System.Management.Automation.PSCredential -ArgumentList @($localusername,(ConvertTo-SecureString -String $password -AsPlainText -Force))
+
+    start-process "$productPath\$productExec" -ArgumentList $argList -wait -Credential $credentials
+    start-process "$productPath\$productExec" -ArgumentList $argList -wait
+    #>
+
+    choco install docker-desktop --pre
+
+    Add-LocalGroupMember -Group "docker-users" -Member $localusername;
+
+    #enable kubernets mode
+    <#
+    $file = "C:\Users\adminfabmedical\AppData\Roaming\Docker\settings.json";
+    $data = get-content $file -raw;
+    $json = ConvertFrom-Json $data;
+    $json.kubernetesEnabled = $true;
+    set-content $file $json;
+    #>
+}
+
+function InstallWSL2
+{
+    write-host "Installing WSL2";
+
+    mkdir c:\temp -ea silentlycontinue
+    cd c:\temp
+    
+    $downloadNotePad = "https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi";
+
+    #download it...		
+    Start-BitsTransfer -Source $DownloadNotePad -DisplayName Notepad -Destination "wsl_update_x64.msi"
+
+    $credentials = New-Object System.Management.Automation.PSCredential -ArgumentList @($localusername,(ConvertTo-SecureString -String $password -AsPlainText -Force))
+
+    #Start-Process msiexec.exe -Wait -ArgumentList '/I C:\temp\wsl_update_x64.msi /quiet' -Credential $credentials
+    Start-Process msiexec.exe -Wait -ArgumentList '/I C:\temp\wsl_update_x64.msi /quiet'
+
+    <#
+    wsl --set-default-version 2
+    wsl --set-version Ubuntu 2
+    wsl --list -v
+    #>
+}
+
+function InstallVisualStudio()
+{
+    write-host "Installing Visual Studio";
+
+    # Install Chocolatey
+    if (!(Get-Command choco.exe -ErrorAction SilentlyContinue)) {
+        Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))}
+        
+        # Install Visual Studio 2019 Community version
+        #choco install visualstudio2019community -y
+
+        # Install Visual Studio 2019 Enterprise version
+        choco install visualstudio2019enterprise -y
+}
+
+function InstallWSL()
+{
+    write-host "Installing WSL";
+
+    dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+    
+    dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
+}
+
+function UpdateVisualStudio($edition)
+{
+    mkdir c:\temp -ea silentlycontinue
+    cd c:\temp
+    
+    Write-Host "Update Visual Studio." -ForegroundColor Yellow
+
+    $Edition = 'Enterprise';
+    $Channel = 'Release';
+    $channelUri = "https://aka.ms/vs/16/release";
+    $responseFileName = "vs";
+ 
+    $intermedateDir = "c:\temp";
+    $bootstrapper = "$intermedateDir\vs_$edition.exe"
+    #$responseFile = "$PSScriptRoot\$responseFileName.json"
+    #$channelId = (Get-Content $responseFile | ConvertFrom-Json).channelId
+    
+    $bootstrapperUri = "$channelUri/vs_$($Edition.ToLowerInvariant()).exe"
+    Write-Host "Downloading Visual Studio 2019 $Edition ($Channel) bootstrapper from $bootstrapperUri"
+
+    $WebClient = New-Object System.Net.WebClient
+    $WebClient.DownloadFile($bootstrapperUri,$bootstrapper)
+
+    #& $bootstrapper update --quiet
+
+    Start-Process $bootstrapper -Wait -ArgumentList 'update --quiet'
+
+    #update visual studio installer
+    #& "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vs_installer.exe" update --quiet
+
+    #update visual studio
+    #& "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vs_installer.exe" update  --quiet --norestart --installPath 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise'
+
+    #& $bootstrapper update  --quiet --norestart --installPath 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise'
+
+    Start-Process $bootstrapper -Wait -ArgumentList "update --quiet --norestart --installPath 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise'"
 }
 
 #Disable-InternetExplorerESC
@@ -265,28 +445,52 @@ DisableInternetExplorerESC
 
 EnableIEFileDownload
 
-InstallAzPowerShellModule
-
-InstallNotepadPP
-
-$ext = @("ms-vscode.azurecli")
-InstallVisualStudioCode $ext
-
 InstallDotNetCore "3.1"
 
 InstallDotNet5;
 
 InstallGit;
 
-InstallChocolaty
+DisableInternetExplorerESC
+
+EnableIEFileDownload
+
+InstallChocolaty;
 
 InstallFiddler;
 
 InstallPostman;
 
-InstallAzureCli;
+InstallPutty
+
+InstallGit
+
+InstallAzureCli
 
 InstallPorter;
+
+InstallChrome
+
+InstallNotepadPP
+
+InstallAzPowerShellModule
+
+InstallWSL
+
+InstallWSL2
+
+InstallDockerDesktop
+
+InstallUbuntu
+
+InstallVisualStudioCode
+
+$ext = @("ms-vscode.azurecli")
+InstallVisualStudioCode $ext
+
+InstallVisualStudio "enterprise"
+
+UpdateVisualStudio "enterprise"
 
 CreateLabFilesDirectory
 
